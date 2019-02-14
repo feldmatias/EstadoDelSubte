3f83b92b27faee678cbfce13e3d2b5322d8de735
matias feld <matias.feld@mdevz.com>
2019-02-14 14:21:02 -0300

    updated curl calls

--- Server/app/Http/Controllers/SubwayController.php
+++ Server/app/Http/Controllers/SubwayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Subway;
 use Sunra\PhpSimple\HtmlDomParser;
+use Ixudra\Curl\Facades\Curl;
 
 class SubwayController extends Controller
 {
@@ -18,30 +19,29 @@ class SubwayController extends Controller
     }
 
     public function check(){
-    	$web = $this->getHtmlFromUrl();
-    	$html = HtmlDomParser::str_get_html($web);
-    	$subways = Subway::get();
-    	foreach ($subways as $subway){
-    		if ($html){
-    			$container = 'span[id=status-line-' . $subway->line . ']'; //format == <span id='status-line-A'>State</span>
-    			$newState = $html->find($container, 0)->innertext;
-    			$subway->store($newState);
-    		} else {
-    			$subway->storeError();
-    		}
-    	}
+        $response = Curl::to(self::url)->returnResponseObject()->get();
+
+        if ($response->status != 200){
+            return $this->pageError();
+        }
+
+        $this->storeNewState($response->content);
     }
 
-    private function getHtmlFromUrl(){
-    	$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, self::url);
-		curl_setopt($curl, CURLOPT_FAILONERROR, true);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		$web = curl_exec($curl);
-		if (curl_error($curl)){
-		    $web = '';
-		}
-		curl_close($curl);
-		return $web;
+    private function pageError(){
+    	$subways = Subway::get();
+        foreach ($subways as $subway){
+            $subway->storeError();
+        }
+    }
+
+    private function storeNewState($web){
+        $html = HtmlDomParser::str_get_html($web);
+        $subways = Subway::get();
+        foreach ($subways as $subway){
+            $container = 'span[id=status-line-' . $subway->line . ']'; //format == <span id='status-line-A'>State</span>
+            $newState = $html->find($container, 0)->innertext;
+            $subway->store($newState);
+        }
     }
 }
